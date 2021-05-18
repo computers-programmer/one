6e98df7c1edd943c2c2ff367a24de3e77b94015f
unknown <kcomputer.programmer1@gmail.com>
2021-05-18 18:57:38 +0530

    second sum fill is changed

--- sum.py
+++ sum.py
@@ -1,6 +1,13 @@
 a=int(input("enter a number "))
 b=int(input("enter a number "))
 
+
+a=int(input("enter a number "))
+b=int(input("enter a number "))
+a=int(input("enter a number "))
+b=int(input("enter a number "))a=int(input("enter a number "))
+b=int(input("enter a number "))
+
 c=a+b
 print(c)
 c=a-b
